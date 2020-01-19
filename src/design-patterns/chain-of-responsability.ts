0ed5b6c9d4cbd3d33e77919f68c0e8ef30886a2d
onnecfactory <59940935+onnecfactory@users.noreply.github.com>
2020-01-19 13:41:48 -0500

    Chain Of Responsability

---
+++ src/design-patterns/chain-of-responsability.ts
@@ -0,0 +1,63 @@
+interface Handler {
+  setNext(h: Handler): Handler;
+  handle(request: string): void;
+}
+
+abstract class BaseHandler implements Handler {
+  next: Handler;
+  constructor() {
+    this.next = null;
+  }
+  setNext(h: Handler): Handler {
+    this.next = h;
+    return this.next;
+  }
+  handle(request: string): void {
+    if (this.next !== null) this.next.handle(request);
+  }
+}
+
+class HandlerA extends BaseHandler {
+  handle(request: string): void {
+    console.log("Handler A");
+    if (request === "A") {
+      console.log("Processing request...");
+    } else {
+      console.log("Seding request to the next Handler...");
+      super.handle(request);
+    }
+  }
+  gg(): void {}
+}
+
+class HandlerB extends BaseHandler {
+  handle(request: string): void {
+    console.log("Handler B");
+    if (request === "B") {
+      console.log("Processing request...");
+    } else {
+      console.log("Seding request to the next Handler...");
+      super.handle(request);
+    }
+  }
+}
+
+class HandlerC extends BaseHandler {
+  handle(request: string): void {
+    console.log("Handler C");
+    if (request === "C") {
+      console.log("Processing request...");
+    } else {
+      console.log("Seding request to the next Handler...");
+      super.handle(request);
+    }
+  }
+}
+
+const h1 = new HandlerA();
+const h2 = new HandlerB();
+const h3 = new HandlerC();
+
+h1.setNext(h2).setNext(h3);
+
+h1.handle("C");
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
 //import "./design-patterns/mediator-pattern";
-import "./design-patterns/observer-pattern";
+//import "./design-patterns/observer-pattern";
+import "./design-patterns/chain-of-responsability";
 
 import App from "./App";
 
